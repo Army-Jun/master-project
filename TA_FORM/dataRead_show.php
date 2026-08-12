ca08b6fa766eeca1632cda3691713ea86a929b61
Army-Jun <80247449+Army-Jun@users.noreply.github.com>
2026-08-12 23:11:23 +0800

    Delete TA_FORM directory

--- TA_FORM/dataRead_show.php
+++
@@ -1,5 +0,0 @@
-<?php
-	$recordsFolderPath = './records';
-	$datatxtPath = $recordsFolderPath . "/" . $_GET['studentID'] . "/" . $_GET['fileItem'];
-	echo "<object data = '" . $datatxtPath . "'>Not supported </object>";
-?>
